e6003f539911e648bb8931704114aab57633cc96
Muhammet Akkurt <muhammetakkurt@Muhammets-MacBook-Air.local>
2016-10-25 12:35:08 +0200

    test

--- platforms/browser/www/js/config.js
+++
@@ -1,8 +0,0 @@
-
-var krms_config ={	
-	'ApiUrl' : "http://acliginigider.com/mobileapp/api",
-	'DialogDefaultTitle' : "Açlığını Gider",
-	'pushNotificationSenderid' : "47016468986",
-	'facebookAppId' : "1067021110056199",
-	'APIHasKey' : "dbfd8b185d4c457398217ead219bc2ee"
-};
